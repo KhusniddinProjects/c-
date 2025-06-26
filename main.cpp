847853e03a7c1edbe08ad6dee3a1d9f72d7834f8
KhusniddinProjects <k.rakhmonov98@gmail.com>
2025-06-26 21:31:18 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    /*
+    /// 1 kg konfet narxi berilgan (haqiqiy son). 1.1, 1.2, …, 2 kg konfet narxlarini
+    /// ekranga chiqaruvchi programma tuzilsin.
+    float konfet;
+    cout << "Konfet narxini kiriting: "; cin >> konfet;
+    for (float i = 1.1; i <=2; i+=0.1) {
+        cout << i << " "<< i*konfet<<endl;
+    }
+    */
+
+    /// 0 dan N sonigacha bo’lgan barcha butun sonlarni
+    /// ildizlarini ekranga chiqaruvchi dastur tuzing (N>1).
+/*
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << i << " "<<sqrt(i)<<endl;
+    }
+    */
+
+    return 0;
+}
