3d9104c96e67f030795f570948a4a6f951f6af50
KhusniddinProjects <k.rakhmonov98@gmail.com>
2025-06-26 21:42:46 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main() {
-    /*
-    /// 1 kg konfet narxi berilgan (haqiqiy son). 1.1, 1.2, …, 2 kg konfet narxlarini
-    /// ekranga chiqaruvchi programma tuzilsin.
-    float konfet;
-    cout << "Konfet narxini kiriting: "; cin >> konfet;
-    for (float i = 1.1; i <=2; i+=0.1) {
-        cout << i << " "<< i*konfet<<endl;
-    }
-    */
-
-    /// 0 dan N sonigacha bo’lgan barcha butun sonlarni
-    /// ildizlarini ekranga chiqaruvchi dastur tuzing (N>1).
+    /// Ikkita a va b soni berilgan. Berilgan sonlarni qaysi birini
+    /// bo'luvchilari ko'pligini topuvchi dastur tuzing.
 /*
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cout << i << " "<<sqrt(i)<<endl;
+    int a,b; cout<<"Enter a number:";cin>>a;
+    cout<<"Enter another number:";cin>>b;
+    int miqdorA=0,miqdorB=0;
+    for (int i=1; i<=a; i++) {
+        if (a%i==0) {
+            cout<<i<<" "; miqdorA++;
+        }
     }
+    for (int i=1; i<=b; i++) {
+        if (b%i==0) {
+            cout<<i<<" "; miqdorB++;
+        }
+    }
+    if (miqdorA==miqdorB) {
+        cout<<"\n ikki sonda ham bo'luvchilar teng " <<endl;
+       }
+    else if ( miqdorA>miqdorB) {
+        cout <<" \n birinchi sonning bo'luvchilari ko'p "<< endl;
+    }
+    else if ( miqdorA<miqdorB) {
+                cout<< " \n ikkinchi sonning bo'luvchilari ko'p "<< endl;
+            }
+            */
+
+    /// Ikkta a va b sonlari berilgan. a soni b dan kichik a dan b gacha bo'lgan sonlarni
+    /// yeg'indisi aniqlovchi dastur tuzing. ( a va b  yegindiga kirmasin ).
+/*
+    int a,b;
+    cout<<"Enter two numbers"; cin>>a>>b;
+    int sum=0;
+    for (int i=a; i<=b; i++) {
+        cout << i<< " ";sum+=i;
+    }
+    cout<<"\n "<< sum<<endl;
     */
 
     return 0;
