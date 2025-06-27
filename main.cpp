ff198306893737ef46fc3e6eadc3f22dd5e93e9c
KhusniddinProjects <k.rakhmonov98@gmail.com>
2025-06-27 09:37:41 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 using namespace std;
 int main() {
+    /// a soni berilgan. Shu sonni tublikga tekshiring. Agar tub bo'lsa true,
+    /// aks holda false chiqsin.
     /*
-    /// 1 kg shakar 4000 so'm bo'lsa. 13, 14, 15, 16, 17, ..... 55, 56, 57, 58, 59, 60 kg
-    /// shakar qancha bo'lishini aniqlang.
-    int sum=4000;
-    for (int i = 1; i <= 60; i++) {
-        cout << i << " "<< i*sum<<endl;
-
+    int a; cout << "Enter a number: "; cin >> a;
+    int tub = 0;
+    for (int i = 1; i <= a; i++) {
+        if ( a%i == 0) {
+            cout << i << " ";tub++;
+        }
     }
-    */
-    /*
-    /// 35 dan n gacha bo'lgan 3 ga bo'linmaydigan sonlarni ekranga chiqaring
-    /// va ularni sonini topuvchi dastur tuzing
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    for (int i = 35; i <= n; i++) {
-        if (i % 3 !=0) cout << i << " ";
+    if (tub == 2) {
+        cout << "\n True" <<endl;
+    } else cout << "\n False" <<endl;
+*/
+    /// a va b soni berilgan. Agar 2 lasi ham tub bo'lsa ularni summasini,
+    /// aks holda ko'paytmasini chiqaring.
+/*
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    int tubA = 0;
+    int tubB = 0;
+    for (int i=1; i<=a; i++) {
+        if (a%i==0) {
+            cout<<i<<" ";tubA++;
+        }
     }
-    for (int i = 35; i>=n ; i--) {
-        if (i%3!=0) cout << i << " ";
+    for (int i=1; i<=b; i++) {
+        if (b%i==0) {
+            cout<<i<<" ";tubB++;
+        }
     }
-    */
-
+    if ( tubA==2 && tubB==2 ) cout<<" \n ikkisi ham tub "<< a+b<<endl;
+    else if (tubA!=2 && tubB==2) cout<<"\n birinchi son tub emas va boshqasi tub "<< a*b<<endl;
+    else if (tubA==2 && tubB!=2) cout<<"\n birinchi son tub va boshqasi tub emas "<< a*b<<endl;
+    else if (tubA!=2 && tubB!=2) cout<<"\n ikkisi ham juft "<< a << " va "<< b<<endl;
+*/
 
     return 0;
 }
