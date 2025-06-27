9d44b94d2ea3d7ab735ba4941d097fad6e4ba5d1
KhusniddinProjects <k.rakhmonov98@gmail.com>
2025-06-27 09:28:43 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,44 +1,29 @@
 #include <iostream>
 using namespace std;
 int main() {
-    /// Ikkita a va b soni berilgan. Berilgan sonlarni qaysi birini
-    /// bo'luvchilari ko'pligini topuvchi dastur tuzing.
-/*
-    int a,b; cout<<"Enter a number:";cin>>a;
-    cout<<"Enter another number:";cin>>b;
-    int miqdorA=0,miqdorB=0;
-    for (int i=1; i<=a; i++) {
-        if (a%i==0) {
-            cout<<i<<" "; miqdorA++;
-        }
-    }
-    for (int i=1; i<=b; i++) {
-        if (b%i==0) {
-            cout<<i<<" "; miqdorB++;
-        }
-    }
-    if (miqdorA==miqdorB) {
-        cout<<"\n ikki sonda ham bo'luvchilar teng " <<endl;
-       }
-    else if ( miqdorA>miqdorB) {
-        cout <<" \n birinchi sonning bo'luvchilari ko'p "<< endl;
-    }
-    else if ( miqdorA<miqdorB) {
-                cout<< " \n ikkinchi sonning bo'luvchilari ko'p "<< endl;
-            }
-            */
+    /*
+    /// 1 kg shakar 4000 so'm bo'lsa. 13, 14, 15, 16, 17, ..... 55, 56, 57, 58, 59, 60 kg
+    /// shakar qancha bo'lishini aniqlang.
+    int sum=4000;
+    for (int i = 1; i <= 60; i++) {
+        cout << i << " "<< i*sum<<endl;
 
-    /// Ikkta a va b sonlari berilgan. a soni b dan kichik a dan b gacha bo'lgan sonlarni
-    /// yeg'indisi aniqlovchi dastur tuzing. ( a va b  yegindiga kirmasin ).
-/*
-    int a,b;
-    cout<<"Enter two numbers"; cin>>a>>b;
-    int sum=0;
-    for (int i=a; i<=b; i++) {
-        cout << i<< " ";sum+=i;
     }
-    cout<<"\n "<< sum<<endl;
     */
+    /*
+    /// 35 dan n gacha bo'lgan 3 ga bo'linmaydigan sonlarni ekranga chiqaring
+    /// va ularni sonini topuvchi dastur tuzing
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 35; i <= n; i++) {
+        if (i % 3 !=0) cout << i << " ";
+    }
+    for (int i = 35; i>=n ; i--) {
+        if (i%3!=0) cout << i << " ";
+    }
+    */
+
 
     return 0;
 }
