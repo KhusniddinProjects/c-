701cbdba6b1d3db358b1811b42c33d412f3e273b
KhusniddinProjects <k.rakhmonov98@gmail.com>
2025-07-10 16:42:23 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,86 +1,55 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main() {
-    /// A Dan B gacha tub sonlar ekranga chiqarish
     /*
-    for (int i = 50; i <= 100; i++) {
-       int sanoq=0;
-       for (int j = 1; j <= i; j++) {
-           if (i % j == 0) sanoq++;
-       }
-       if (sanoq ==2) cout << i << " ";
-   }
-   */
-
-    /*
-    /// Sonning necha xonali ekanini topish
-    int son; cout << "Enter a number: "; cin >> son;
-    int sanoq=0;
-    while(true) {
-        son = son /10;
-        ++sanoq;
-        if(son==0) break;
-    }
-    cout << " Natija: "<< sanoq << " xonali son"<< endl;
+    /// Berilgan str satridagi kichik harflar va raqamlarni birgalikdagi
+    /// sonini aniqlo'vchi dastur tuzing.
+    string s;
+    cout<<"Enter string: "; getline(cin, s);
+    int strLen = s.length();
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < strLen; i++) {
+        if (islower(s[i])) ++a;
+        if (isdigit(s[i])) ++b;
+    }  cout<<" kichik harflar "<<a<<endl;
+       cout<<" raqamlar "<<b<<endl;
+       cout << " birgalikda " << a+b<<endl;
     */
 
-    /// Sonning juft raqamlarini topish
     /*
-    int son,birlar;
-    cout<<" Enter a number: "; cin>>son;
-    int sanoq=0;
-    while(true) {
-        birlar=son % 10;
-        if ( son % 2 == 0) ++sanoq;
-        son/=10;
-        if (son == 0) break;
-    } cout << " bu sonda "<<sanoq <<" ta juft son bor"<< endl;
+    /// Berilgan str satridagi katta harflar va raqamlarni
+    /// birgalikdagi sonini aniqlo'vchi dastur tuzing.
+    string s;
+    cout<<"Enter string: "; getline(cin, s);
+    int strLen = s.length();
+    int a = 0, b = 0;
+    for (int i = 0; i < strLen; i++) {
+        if (isupper(s[i])) ++a;
+        if (isdigit(s[i])) ++b;
+    } cout << " katta harflar " << a << endl;
+    cout << " raqamlar " << b << endl;
+    cout << " birgalikda " << a + b << endl;
     */
 
-    /// Sonlar kiritish va 0 kirtilganda to'xtash, va ungacha nechta son kiritilganini sanash
     /*
-    int son, sanoq;
-
-    while(true) {
-        cout<< "Sonni kiriting: "; cin >> son;
-        if ( son == 0 ) break;
-        ++sanoq;
-    } cout << "Natija : "<< sanoq << endl;
+    // Berilgan str satridagi katta harflar, kichik harflar, raqamlar va belgilarni
+    // sonini alohida aniqlo'vchi dastur tuzing.
+    string s;
+    cout<<"Enter a string: "; getline(cin, s);
+    int strLen = s.length();
+    int a=0, b=0, c=0, d=0;
+    for(int i=0; i<strLen; i++) {
+        if (isupper(s[i])) ++a;
+        else if (islower(s[i])) ++b;
+        else if (isdigit(s[i])) ++c;
+        else if (!isupper(s[i])&& !islower(s[i]) &&!isdigit(s[i])) ++d;
+    } cout << " katta harflar "<<a<< endl;
+     cout << " kichik harflar "<<b<< endl;
+     cout << " raqamlar "<<c<< endl;
+     cout << " belgilar " <<d<< endl;
     */
-
-    /// Faqat 50 Dan katta sonlar yig'indisini topish
-    /*
-    int son, summa;
-    while ( true) {
-        cout<< " Enter a number: "; cin >> son;
-        if (son == 0) break;
-        if ( son> 50) summa+=son;
-    }  cout << "Natija : "<< summa << endl;
-    */
-
-    /// Sonlar ichida 5 raqami borligini aniqlash
-    /*
-    int son, birlar;
-    cout<<"Please enter a number: "; cin>>son;
-    int sanoq=0;
-    while (true) {
-        birlar = son % 10;
-        if (birlar == 5) ++sanoq;
-        son/=10;
-        if (son==0) break;
-    } if (sanoq>0) cout<< " 5 raqami mavjud \n";
-    else cout<< " 5 raqami mavjud emas \n";
-    */
-    /*
-    /// Berilgan son 3 Ning darajasi yoki yo'qligini aniqlash
-    int n;
-    cout << "Enter a number: "; cin >> n;
-    int i=1;
-    while (n > i) {
-        i*=3;
-    } if ( i==n) cout << " 3 ning darajasi \n";
-    else cout << " 3 ning darajasi emas \n";
-    */
-    
     return 0;
+
 }
